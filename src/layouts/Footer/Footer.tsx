35aff45ecc486baaa40370fc205fbcd5d34f208d
Hayden Buckley-Smith <hayden@abstack.co>
2022-01-31 12:08:10 +0000

    refactor: Layout Changes - Imported a placeholder Image into header - Refactored Header classes and added pipes to separate Header links - Imported Zap icon and Separator component into footer - Amended background colours to new custom color token - Created a mailto link to my email - Added social media icons (Github, LinkedIn, Instagram, Twitter) - Set hover states for all links in Header and Footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,34 +1,41 @@
-import { GitHub, Instagram, Linkedin, Twitter } from "react-feather";
+import { GitHub, Instagram, Linkedin, Twitter, Zap } from "react-feather";
+import { Separator } from "../../components";
 
 interface FooterProps {
   className?: string;
 }
 
 export const Footer = ({ className }: FooterProps) => {
-  const classes = ["text-blue12", className].join(" ").trim();
+  const classes = ["text-indigo12 p-2", className].join(" ").trim();
   return (
     <footer className={classes}>
-      <section className="flex justify-between">
+      <Separator className="mt-4" />
+      <section className="flex justify-between mt-4">
         <div className="flex space-x-4">
-          <a href="https://github.com/hayden-93" className="hover:text-blue11">
+          <a href="https://github.com/hayden-93" className="text-indigo12 hover:text-indigo11">
             <GitHub />
           </a>
-          <a href="https://uk.linkedin.com/in/hayden-buckley-smith-b14070142" className="hover:text-blue11">
+          <a
+            href="https://uk.linkedin.com/in/hayden-buckley-smith-b14070142"
+            className="text-indigo12 hover:text-indigo11"
+          >
             <Linkedin />
           </a>
-          <a href="https://www.instagram.com/hbsdev/" className="hover:text-blue11">
+          <a href="https://www.instagram.com/hbsdev/" className="text-indigo12 hover:text-indigo11">
             <Instagram />
           </a>
-          <a href="https://twitter.com/HBSDev" className="hover:text-blue11">
+          <a href="https://twitter.com/HBSDev" className="text-indigo12 hover:text-indigo11">
             <Twitter />
           </a>
         </div>
-        <div>
-          <span>rfjhberfhgeruighequrhgeqru</span>
+        <div className="flex space-x-4">
+          <Zap />
+          <span>Designed and Built by Hayden Buckley-Smith</span>
+          <Zap />
         </div>
         <div>
           <div>
-            <a href="mailto:hayden@abstack.co" className="hover:text-blue11">
+            <a href="mailto:hayden@abstack.co" className="text-indigo12 hover:text-indigo11">
               hayden@abstack.co
             </a>
           </div>
--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,32 +1,38 @@
+import Image from "next/image";
+import profilePic from "../../../public/placeholder-image.png";
+
 interface HeaderProps {
   className?: string;
 }
 
 export const Header = ({ className }: HeaderProps) => {
-  const classes = ["mt-4 text-blue12", className].join(" ").trim();
+  const classes = ["mt-4 text-indigo12", className].join(" ").trim();
 
   return (
     <header className={classes}>
-      <div className="flex justify-between">
-        <h1>Hayden Buckley-Smith</h1>
-        <ul className="flex space-x-4">
+      <div className="flex justify-between items-center">
+        <Image src={profilePic} alt="Picture of the author" width={100} height={100} className="bg-indigo12" />
+        <ul className="flex space-x-12">
           <li>
-            <a href="#" className="hover:text-blue11">
+            <a href="#" className="text-indigo-12 hover:text-indigo11">
               Home
             </a>
           </li>
+          <span>|</span>
           <li>
-            <a href="#" className="hover:text-blue11">
+            <a href="#" className="text-indigo-12 hover:text-indigo11">
               About Me
             </a>
           </li>
+          <span>|</span>
           <li>
-            <a href="#" className="hover:text-blue11">
+            <a href="#" className="text-indigo-12 hover:text-indigo11">
               Blog
             </a>
           </li>
+          <span>|</span>
           <li>
-            <a href="#" className="hover:text-blue11">
+            <a href="#" className="text-indigo-12 hover:text-indigo11">
               Contact Me
             </a>
           </li>
